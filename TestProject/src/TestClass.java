f13e1ebfa95fe2ac1f71366c9cee5349ac259339
Admin <Admin@Admin-PC>
2018-12-19 17:40:21 +0530

    updated

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -3,6 +3,6 @@ public class TestClass {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Test Class in execution right now");
+		System.out.println("Test Class in execution right now to test");
 	}
 }
